e126868d3d14000f37349a94571228322fa38850
Karsten Ahnert <karsten.ahnert@gmx.de>
2013-05-06 22:12:33 +0200

    ...

---
+++ gotw_1.cpp
@@ -0,0 +1,18 @@
+/*
+ * gotw_1.cpp
+ * Date: 2013-05-06
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ */
+
+#include <iostream>
+
+#define tab "\t"
+
+using namespace std;
+
+
+int main( int argc , char *argv[] )
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
